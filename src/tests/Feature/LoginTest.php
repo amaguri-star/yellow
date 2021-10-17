898a296f36e8dc5fddec28b2341fdff3cdba6609
Ryusei <ryusei1597@gmail.com>
2021-10-18 00:54:54 +0900

    add tests test_user_can_view_a_login_form and test_user_can_login_with_correct_credentials

--- src/tests/Feature/LoginTest.php
+++ src/tests/Feature/LoginTest.php
@@ -2,21 +2,41 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_can_view_a_login_form()
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/login');
         $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('i-love-laravel'),
+        ]);
+
+        $this->assertFalse(Auth::check());
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'i-love-laravel',
+        ]);
+
+        $this->assertTrue(Auth::check());
+        $response->assertRedirect('/');
     }
 }
