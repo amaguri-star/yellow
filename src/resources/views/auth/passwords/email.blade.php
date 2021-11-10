abd82bec1bda4b042e89e964b18578e4b579f19a
Ryusei <ryusei1597@gmail.com>
2021-11-10 09:38:06 +0900

    created email.blade template for sendering rest link to user

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -3,5 +3,25 @@
 @section('title', 'パスワード再設定')
 
 @section('content')
-    <p>インプットフォーム</p>
+    <div class="container mt-5">
+        <div class="row d-flex justify-content-center">
+            <div class="card col-7">
+                <div class="card-body text-center">
+                    <h2 class="card-title">パスワード再設定</h2>
+                    <hr>
+                    @include('error_card_list')
+                    <div class="card-text">
+                        <form action="{{ route('password.request') }}" method="POST">
+                            @csrf
+                            <div class="md-form mb-4">
+                                <input type="email" id="email" name="email" class="form-control"
+                                    value="{{ old('email') }}" placeholder="email" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">送信</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
