a1addeec760430229cbd4014779018b906df1b9a
Ryusei <ryusei1597@gmail.com>
2021-11-13 12:02:22 +0900

    create index

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,32 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
+
 class TaskController extends Controller
 {
     public function index()
     {
-        // add code later
+        $tasks = Task::where('user_id', Auth::id())->get();
+        return view('tasks', ['tasks' => $tasks]);
     }
+
     public function store()
     {
         // add code later
     }
+
     public function create()
     {
         // add code later
     }
+
     public function show()
     {
         // add code later
     }
+
     public function update()
     {
         // add code later
     }
+
     public function destroy()
     {
         // add code later
     }
+    
     public function edit()
     {
         // add code later
