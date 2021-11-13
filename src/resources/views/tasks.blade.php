041e4ae09901c0ad64aca4105a3b0bb35195fd3f
Ryusei <ryusei1597@gmail.com>
2021-11-13 12:52:45 +0900

    create showing tasks for user in blade

---
+++ src/resources/views/tasks.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('title', '完了タスク一覧')
+
+@section('content')
+<div class="container my-5 px-5">
+    @foreach ($tasks as $task)
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5>{{ $task->name }}</h5>
+                <p class="card-text">{{ $task->description }}</p>
+            </div>
+        </div>
+    @endforeach
+</div>
+@endsection
