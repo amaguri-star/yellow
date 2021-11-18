3a2d5ecb44b9c7730e848a7ccf6e733ee77d10ee
Ryusei <ryusei1597@gmail.com>
2021-11-18 15:04:45 +0900

    created delete modal

--- src/resources/views/tasks.blade.php
+++ src/resources/views/tasks.blade.php
@@ -3,15 +3,20 @@
 @section('title', '完了タスク一覧')
 
 @section('content')
-@include('flush_message')
-<div class="container my-5 px-5">
-    @foreach ($tasks as $task)
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5>{{ $task->name }}</h5>
-                <p class="card-text">{{ $task->description }}</p>
+    @include('flush_message')
+    <div class="container my-5 px-5">
+        @foreach ($tasks as $task)
+            <div class="card mb-3">
+                <div class="card-body d-flex justify-content-between">
+                    <div class="card-content">
+                        <h5>{{ $task->name }}</h5>
+                        <p class="card-text">{{ $task->description }}</p>
+                    </div>
+                    <div class="delete-button">
+                        @include('modal')
+                    </div>
+                </div>
             </div>
-        </div>
-    @endforeach
-</div>
+        @endforeach
+    </div>
 @endsection
