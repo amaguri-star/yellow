f35771bc575986f2bcb2a2612b2248aa2dbd1dc8
Ryusei <ryusei1597@gmail.com>
2021-12-17 20:06:26 +0900

    modify html for replacing flush messaging

--- src/resources/views/tasks.blade.php
+++ src/resources/views/tasks.blade.php
@@ -3,20 +3,22 @@
 @section('title', '完了タスク一覧')
 
 @section('content')
-    @include('flush_message')
-    <div class="container my-5 px-5">
-        @foreach ($tasks as $task)
-            <div class="card mb-3">
-                <div class="card-body d-flex justify-content-between">
-                    <div class="card-content">
-                        <h5>{{ $task->name }}</h5>
-                        <p class="card-text">{{ $task->description }}</p>
-                    </div>
-                    <div class="delete-button">
-                        @include('modal')
+    <div class="tasks__content w-100">
+        @include('flush_message')
+        <div class="my-5 px-5">
+            @foreach ($tasks as $task)
+                <div class="card mb-3">
+                    <div class="card-body d-flex justify-content-between">
+                        <div class="card-content">
+                            <h5>{{ $task->name }}</h5>
+                            <p class="card-text">{{ $task->description }}</p>
+                        </div>
+                        <div class="delete-button">
+                            @include('modal')
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
