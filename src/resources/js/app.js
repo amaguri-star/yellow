2d37be9db5f9bf49ff65d033f7f0f27e06850c96
Ryusei <ryusei1597@gmail.com>
2022-01-02 12:15:09 +0900

    create component TaskList

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,11 +2,13 @@ import './bootstrap'
 import Vue from 'vue'
 import Vuetify from './plugins/vuetify'
 import VueDatePicker from './components/VueDatePicker'
+import TaskList from './components/TaskList'
 
 const app = new Vue({
     el: '#app',
     vuetify: Vuetify,
     components: {
         VueDatePicker,
+        TaskList,
     }
 })
