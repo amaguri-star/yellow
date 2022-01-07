b27bc9611eb53671bef185dea5790c9bb980c639
Ryusei <ryusei1597@gmail.com>
2022-01-07 09:37:24 +0900

    delete ununsed file

--- src/resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-
-@section('title', 'ホーム')
-
-@section('content')
-    <h1>ホーム</h1>
-@endsection
--- src/resources/views/tasks.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-
-@section('title', '完了タスク一覧')
-
-@section('content')
-    <task-list :tasks="{{ $tasks }}"></task-list>
-@endsection
